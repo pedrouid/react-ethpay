82d8d8a98206eca1b63a52de99152848580106e3
Pedro Gomes <pedro@loot.io>
2017-07-05 02:24:49 +0100

    major cleanup

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import Wrapper from '../components/Wrapper';
-import Column from '../components/Column';
+
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const NotFound = () => (
-  <Wrapper>
-    <Column>
-      <Link to="/"><h1>404 Page Not Found</h1></Link>
-    </Column>
-  </Wrapper>
+  <StyledContainer>
+    <Link to="/"><h1>404 Page Not Found</h1></Link>
+  </StyledContainer>
 );
+
 export default NotFound;
